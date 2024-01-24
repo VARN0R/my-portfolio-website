018de5bd2584d74175e174db298f413531a2e94e
Vladislav <vladislav.malchevskiy.work@gmail.com>
2024-01-24 21:40:51 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector('.hamburger');
+const close = document.querySelector('.menu__close');
+const menu = document.querySelector('.menu');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+})
+
+close.addEventListener('click', () => {
+    menu.classList.remove('active');
+})
+
+
+
+
