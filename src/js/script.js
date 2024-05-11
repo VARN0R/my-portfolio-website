4b3591935d39303f23aa418e281e48542c7b77d8
Vladislav <vladislav.malchevskiy.work@gmail.com>
2024-05-11 12:47:16 +0300

    add animations in the exp section, change favicon

--- src/js/script.js
+++ src/js/script.js
@@ -14,6 +14,14 @@ close.addEventListener('click', () => {
     menu.classList.remove('active');
 })
 
+//Close menu when the user clicked outside the menu
+menu.addEventListener('click', (event) => {
+    if (!event.target.classList.contains("menu__block") && menu.classList.contains("active")) {
+        menu.classList.remove('active');
+    }
+})
+
+
 //Close menu when the user clicked on the link
 for (let i = 0; i < menuLink.length; i++) {
     menuLink[i].addEventListener('click', function (e) {
@@ -146,6 +154,89 @@ myForm.addEventListener('submit', (e) => {
     e.preventDefault();
     formSend(e);
 })
+
+
+
+//Add animation to stick in exp section
+
+const expWrapper = document.querySelector(".exp__wrapper");
+expWrapper.style.opacity = "0";
+
+let lowestElement = document.getElementById("lowestElementInExpSection");
+let eventHappened = false;
+const sticks = document.querySelectorAll(".animation-stick");
+
+function playHorizontalStickAnimations(index) {
+    let stickAnimation = document.querySelector(`.exp__item_anim${index}`).animate([
+        {width: "0"},
+        {width: "45px"},
+    ], {
+        duration: 250,
+        iterations: 1,
+        pseudoElement: '::before',
+        easing: 'ease-in'
+    });
+    stickAnimation.play();
+    stickAnimation.finished.then(() => {
+        let styleForSticksHorizontal = document.createElement('style');
+        console.log(index);
+        styleForSticksHorizontal.innerHTML = `
+            .exp__item_anim${index}:before {
+                width: 45px;
+            }
+        `;
+        document.head.appendChild(styleForSticksHorizontal);
+    });
+}
+
+function playVerticalStickAnimations(index, indicator) {
+    if (index >= sticks.length && indicator === "experience") return; // Если достигнут конец массива, выходим из функции
+    if (index >= sticks.length / 2 && indicator === "education") return;
+    sticks[index].style.opacity = "1";
+
+    let stickAnimation = sticks[index].animate([
+        {height: "0"},
+        {height: "calc(100% + 16px)"},
+    ], {
+        duration: 500,
+        iterations: 1,
+        pseudoElement: '::after',
+        easing: 'ease-in'
+    });
+
+    stickAnimation.finished.then(() => {
+        playHorizontalStickAnimations(index + 1);
+        playVerticalStickAnimations(index + 1, indicator); // Рекурсивно запускаем следующую анимацию после завершения текущей
+    });
+}
+
+window.addEventListener("scroll", () => {
+    let currentPositionLowestElement = lowestElement.getBoundingClientRect();
+
+    if (currentPositionLowestElement.bottom <= window.innerHeight && !eventHappened) {
+
+        document.querySelectorAll(".exp__item-header").forEach(item => {
+            item.classList.add("animate__animated");
+        })
+        document.querySelectorAll(".exp__item-body").forEach(item => {
+            item.classList.add("animate__animated");
+        })
+
+        eventHappened = true;
+
+        sticks.forEach(item => {
+            item.style.opacity = "0";
+        })
+        expWrapper.style.opacity = "1";
+        setTimeout(() => {
+            playVerticalStickAnimations(0, "education");
+            playVerticalStickAnimations(2, "experience");
+        }, 950);
+    }
+
+})
+
+
 
 
 
